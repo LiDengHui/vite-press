9c4d5081a2a473f2601c3524a8a793eea4e624e1
denghui.li <3379489032@qq.com>
2025-06-06 17:19:01 +0800

    Updates

---
+++ assets/chunks/stateDiagram-v2-YXO3MK2T.yvCy0m6V.js
@@ -0,0 +1,1 @@
+import{s as e,b as r,a,S as s}from"./chunk-AEK57VVT.BqC0VBW9.js";import{_ as i}from"../app.CmkibbgU.js";import"./chunk-RZ5BOZE2.a9UOBiT4.js";import"./framework.DWgr2bMN.js";import"./theme.BfFhAgkW.js";var p={parser:a,get db(){return new s(2)},renderer:r,styles:e,init:i(t=>{t.state||(t.state={}),t.state.arrowMarkerAbsolute=t.arrowMarkerAbsolute},"init")};export{p as diagram};
