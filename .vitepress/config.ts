e83757d35055f0f184de5ebe6829f079deef334a
denghui.li <3379489032@qq.com>
2025-07-19 17:16:17 +0800

    docs: use nginx build

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -73,8 +73,8 @@ const vitePressConfigs: VitePressConfigs = {
         },
         plugins: [
             visualizer({
-                gzipSize: true,
-                brotliSize: true,
+                gzipSize: false,
+                brotliSize: false,
                 emitFile: false,
                 filename: 'test.html', //分析图生成的文件名
                 open: true //如果存在本地服务端口，将在打包后自动展示
